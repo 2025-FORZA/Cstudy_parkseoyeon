8a71c52cfee2b51f38491ca2c4dce44a7e2e8275
parkseoyeon2025 <parkseoyeon2025@sookmyung.ac.kr>
2025-04-05 19:36:40 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,9 @@
+//11382 ²¿¸¶ Á¤¹Î
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+int main() 
+{ long long A, B, C; 
+scanf("%lld %lld %lld", &A, &B, &C); 
+printf("%lld\n", A + B + C); 
+return 0; } 
+
